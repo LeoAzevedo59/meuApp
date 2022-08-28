b5ada8a4ad1b813d31231c06125028bc3173bbf9
Leo Azevedo <leofrancoazevedo@gmail.com>
2022-08-28 20:48:20 -0300

    Componentização

---
+++ src/Components/Pessoa/index.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+class Pessoa extends Component {
+    
+    render() {
+        return (
+            <View style={estilo.area}>
+                <Text style={estilo.textoPessoa}>Nome: {this.props.data.nome}</Text>
+                <Text style={estilo.textoPessoa}>Idade: {this.props.data.idade}</Text>
+                <Text style={estilo.textoPessoa}>E-mail: {this.props.data.email}</Text>
+            </View>
+        )
+    }
+}
+
+const estilo = StyleSheet.create({
+    flex: {
+        flexDirection: 'row',
+    },
+    textoPessoa: {
+        fontSize: 18,
+
+    },
+    area: {
+        paddingTop: 20,
+        marginTop: 20
+    }
+})
+
+export default Pessoa;
